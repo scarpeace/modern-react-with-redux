21995dbc3bae60ed5416d9dc886e779256a10c70
scarpeace <gustavo.scarpellini@gmail.com>
2020-02-19 18:44:20 -0300

    LyfeCycle Methods in React

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,36 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import SeasonDisplay from "./SeasonDisplay"
+import Spinner from "./Spinner"
+
 class App extends React.Component {
 
   constructor(props) {
     super(props)
+  }
 
-    this.state = {
-      latitute: null,
-      errorMessage: ""
-    }
+  state = { latitute: null, errorMessage: "" }
 
-
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => this.setState({ latitute: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })
+    );
   }
 
   render() {
-
-    window.navigator.geolocation.getCurrentPosition(
-      (position) => { this.setState({ latitute: position.coords.latitude }) },
-      (err) => { this.setState({ errorMessage: err.message }) }
-    );
-
 
     if (!this.state.latitute && this.state.errorMessage) {
       return (<div>Error Message:  {this.state.errorMessage} </div>)
     }
     if (this.state.latitute && !this.state.errorMessage) {
-        return (<div>Latitude:  {this.state.latitute} </div>)
+      return <SeasonDisplay lat={this.state.latitute} />
     }
-    
-    return (<div>Loading...</div>)
-    
+    return <Spinner />
   }
 }
 
