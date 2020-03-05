43d8616bd23c9eb8b6cb84216e85630f9b3edd2a
scarpeace <gustavo.scarpellini@gmail.com>
2020-03-05 16:59:13 -0300

    Last commit of the pics project

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import "./ImageList.css"
+import ImageCard from "./ImageCard"
 
 const ImageList = (props) => {
 
-  console.log(props)
-  const images = props.photos.map(({alt_description, id, urls}) => {
-    return <img key={id} src={urls.regular} alt={alt_description}/>
+  const images = props.photos.map((image) => {
+    return <ImageCard key={image.id} image={image}/>
   })
 
   return <div className="image-list">{images}</div>
